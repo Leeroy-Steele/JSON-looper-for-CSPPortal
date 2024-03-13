190dcce7ef4ac14f3226151721e1eada357cd332
Leeroy-Steele <leeroysteele@hotmail.com>
2024-03-13 21:48:48 +1300

    with UI highlighting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,8 +25,6 @@ export default function Home() {
 
     console.log(Object.entries(counts));
     setResult(Object.entries(counts));
-    // console.log(findDuplicate())
-    // console.log(typeof obj)
     setShowResults(true);
   };
 
@@ -42,42 +40,60 @@ export default function Home() {
         <li>(In Dev Tools) Go to network {">"} billing-profile</li>
         <li>(In Dev Tools) Copy the whole billing-profile JSON</li>
         <li>Paste the JSON below</li>
-
       </ol>
-      <form className="mb-6" onSubmit={handleSubmit}>
+      <form className="mb-6 flex flex-col items-center" onSubmit={handleSubmit}>
         <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-          Input JSON from billing-profile (Profile tab)
+          Input JSON from billing-profile
         </label>
         <textarea
           id="message"
           rows={4}
           className="block p-2.5 min-w-96 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="Write your thoughts here..."
+          placeholder="{'id': 63,'productMappings:[]'}"
           onChange={(e) => setObj(JSON.parse(e.target.value))}
         ></textarea>
         <button
-          className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+          className="mt-2 shadow bg-blue-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
           type="submit"
         >
           Show Duplicates
         </button>
       </form>
-      <table 
-        className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400"
-        >
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+      <table className="w-[64rem] text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
-            <th scope="col" className="px-6 py-3">Name</th>
-            <th scope="col" className="px-6 py-3">Count</th>
+            <th scope="col" className="px-6 py-3">
+              Name
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Count
+            </th>
           </tr>
-          {showResults
-            ? result.map((r) => (
-                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                  <td className="px-6 py-4">{r[0]}</td>
-                  <td className="px-6 py-4">{r[1]}</td>
-                </tr>
-              ))
-            : null}
+
+          {/* Show duplicates first */}
+          {showResults &&
+            result.map((r) => 
+              {return r[1]>1&&
+                (<tr
+                key={r[0]}
+                className="bg-yellow-100 border-b dark:bg-gray-800 dark:border-gray-700"
+              >
+                <td className="px-6 py-4">{r[0]}</td><td className="px-6 py-4">{r[1]}</td>
+              </tr>)  
+              })}
+
+          {/* Show the rest */}
+          {showResults &&
+            result.map((r) => 
+              {return r[1]===1&&
+                (<tr
+                key={r[0]}
+                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+              >
+                <td className="px-6 py-4">{r[0]}</td><td className="px-6 py-4">{r[1]}</td>
+              </tr>)  
+              })}
+
         </thead>
       </table>
     </main>
